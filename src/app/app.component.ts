df94d7e4092442de249edcad2f78e4da0a76414f
Man Nguyen <nnman.vn@gmail.com>
2024-05-01 10:12:47 +0800

    feat: reproduce bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, NgZone, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CdkMenuModule } from '@angular/cdk/menu';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CdkMenuModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'zoneless-bug-reproduce';
+  title = '';
+
+  constructor() {
+    if (NgZone.isInAngularZone()) {
+      this.title = 'In Zone';
+    } else {
+      this.title = 'Not In Zone';
+    }
+  }
 }
